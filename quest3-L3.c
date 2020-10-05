bb65061558256b9ccf6a28224331c97aaa01dbfc
vitorbatista-ti <71983471+vitorbatista-ti@users.noreply.github.com>
2020-10-04 21:51:34 -0300

    Add files via upload

---
+++ quest3-L3.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main ( ) {
+
+    char str [50];
+    char str2 [50];
+    printf ("digite o seu nome: ");
+    gets (str);
+    printf ("digite o nome da sua namorada : ");
+    gets (str2);
+    sprintf( resultado, "%s:%s", str1, str2);
+    return 0;
+}
